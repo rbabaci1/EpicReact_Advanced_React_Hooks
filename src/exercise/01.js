dd7e382f90f5f3594c3ac23202d0c1fd3af4e790
rabah babaci <4rabah@gmail.com>
2021-02-23 08:40:07 -0800

    implement useReducer using the dispatch function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,23 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
-
-  function countReducer(currState, newState) {
-    return newState
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count + action.step}
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
   }
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+}
+
+function Counter({step = 1, initialState = 0}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialState,
+  })
+
+  const increment = () => dispatch({type: 'INCREMENT', step})
+
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {

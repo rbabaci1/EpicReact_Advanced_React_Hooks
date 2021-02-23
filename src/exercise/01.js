d7c72be4fdbe6b47779534fb0a4d401d890f750d
rabah babaci <4rabah@gmail.com>
2021-02-23 07:56:40 -0800

    replace the useState with useReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,10 +4,11 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
 
+  function countReducer(currState, newState) {
+    return newState
+  }
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
